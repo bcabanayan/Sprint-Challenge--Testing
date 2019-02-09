6b82864dad90466b61ef09da3b18041b147d4159
Bruce Cabanayan <bcabanayan@Unknown.domain>
2019-02-09 10:51:22 -0700

    Added tests for post endpoint

--- api/server.spec.js
+++ api/server.spec.js
@@ -21,9 +21,32 @@ describe('The route handlers', () => {
         });
 
         it('sends an empty array by default', async () => {
-            const response = await request(server).get('/users');
+            const response = await request(server).get('/games');
 
             expect(response.body).toEqual([]);
         });
     });
+
+    describe('POST /games', () => {
+        it('responds with status code 201', async () => {
+            const body = { title: 'Pacman', genre: 'Arcade', releaseYear: 1980 };
+            const response = await request(server).post('/games').send(body);
+
+            expect(response.status).toBe(201);
+        });
+
+        it('responds with status 422 when body is missing data', async () => {
+            const body = { title: 'Grand Theft Auto' }
+            const response = await request(server).post('/games').send(body);
+
+            expect(response.status).toBe(422);
+        });
+
+        it('responds with an array containing a new id', async () => {
+            const body = { title: 'Super Smash Bros', genre: 'Fighting', releaseYear: 1999 }
+            const response = await request(server).post('/games').send(body);
+
+            expect(response.body).toBe([1]);
+        });
+    }); 
 });
