27fb6f69b3cc3889d16618599f49e2902dc907dd
Bruce Cabanayan <bcabanayan@Unknown.domain>
2019-02-09 10:40:22 -0700

    Wrote tests for get endpoint

--- api/server.spec.js
+++ api/server.spec.js
@@ -6,4 +6,24 @@ describe('The route handlers', () => {
     afterEach(async () => {
         await database('users').truncate();
     });
+
+    describe('GET /games', () => {
+        it('responds with status code 200', async () => {
+            const response = await request(server).get('/games');
+
+            expect(response.status).toBe(200);
+        });
+
+        it('responds with json', async () => {
+            const response = await request(server).get('/games');
+
+            expect(response.type).toMatch(/json/i);
+        });
+
+        it('sends an empty array by default', async () => {
+            const response = await request(server).get('/users');
+
+            expect(response.body).toEqual([]);
+        });
+    });
 });
